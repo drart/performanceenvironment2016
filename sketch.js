084b7f1069228a16ee266660c6a6262dead1a888
Adam Tindale <adam@adamtindale.com>
2016-11-06 09:37:10 -0500

    basic visual effect added. fun

--- sketch.js
+++ sketch.js
@@ -1,11 +1,21 @@
 var container;
-var camera, scene, renderer, parentTransform;
+var camera, scene, renderer, effect;
+var uniforms;
 
 var radius = 100, theta = 0;
 
 function addCube(){
     var ssss = Math.random() * 5; 
+    uniforms = {
+        time:       { value: 1.0 },
+    };
+    var material = new THREE.ShaderMaterial( {
+        uniforms: uniforms,
+        vertexShader: document.getElementById( 'vertexShader' ).textContent,
+        fragmentShader: document.getElementById( 'fragmentShader' ).textContent
+    } );
     cube = new THREE.Mesh( new THREE.CubeGeometry( ssss, ssss, ssss ), new THREE.MeshNormalMaterial() );
+    //cube = new THREE.Mesh( new THREE.CubeGeometry( ssss, ssss, ssss ), material );
     cube.position.y = Math.random() * 200 - 100;
     cube.position.x = Math.random() * 200 - 100;
     cube.position.z = Math.random() * 200 - 100;
@@ -21,77 +31,17 @@ function init() {
     camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000 );
 
     scene = new THREE.Scene();
-    /*
-       var geometry = new THREE.Geometry();
 
-       var point = new THREE.Vector3();
-       var direction = new THREE.Vector3();
-
-       for ( var i = 0; i < 50; i ++ ) {
-
-       direction.x += Math.random() - 0.5;
-       direction.y += Math.random() - 0.5;
-       direction.z += Math.random() - 0.5;
-       direction.normalize().multiplyScalar( 10 );
-
-       point.add( direction );
-
-       geometry.vertices.push( point.clone() );
-
-       }
-
-       parentTransform = new THREE.Object3D();
-       parentTransform.position.x = Math.random() * 40 - 20;
-       parentTransform.position.y = Math.random() * 40 - 20;
-       parentTransform.position.z = Math.random() * 40 - 20;
-
-       parentTransform.rotation.x = Math.random() * 2 * Math.PI;
-       parentTransform.rotation.y = Math.random() * 2 * Math.PI;
-       parentTransform.rotation.z = Math.random() * 2 * Math.PI;
-
-       parentTransform.scale.x = Math.random() + 0.5;
-       parentTransform.scale.y = Math.random() + 0.5;
-       parentTransform.scale.z = Math.random() + 0.5;
-
-       for ( var i = 0; i < 50; i ++ ) {
-
-       var object;
-
-       if ( Math.random() > 0.5 ) {
-
-       object = new THREE.Line( geometry );
-
-       } else {
-
-       object = new THREE.LineSegments( geometry );
-
-       }
-
-       object.position.x = Math.random() * 400 - 200;
-       object.position.y = Math.random() * 400 - 200;
-       object.position.z = Math.random() * 400 - 200;
-
-       object.rotation.x = Math.random() * 2 * Math.PI;
-       object.rotation.y = Math.random() * 2 * Math.PI;
-       object.rotation.z = Math.random() * 2 * Math.PI;
-
-       object.scale.x = Math.random() + 0.5;
-       object.scale.y = Math.random() + 0.5;
-       object.scale.z = Math.random() + 0.5;
-
-       parentTransform.add( object );
-
-       }
-
-       scene.add( parentTransform );
-       */
     renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setClearColor( 0xf0f0f0 );
+    renderer.setClearColor( 0x000000);
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
     container.appendChild(renderer.domElement);
 
     window.addEventListener( 'resize', onWindowResize, false );
+
+    effect = new THREE.AnaglyphEffect( renderer );
+    effect.setSize(window.innerWidth, window.innerHeight);
 }
 
 function onWindowResize() {
@@ -101,6 +51,7 @@ function onWindowResize() {
 
     renderer.setSize( window.innerWidth, window.innerHeight );
 
+    effect.setSize( window.innerWidth, window.innerHeight );
 }
 
 function animate() {
@@ -111,6 +62,7 @@ function animate() {
 
 function render() {
 
+    uniforms.time.value += 0.05;
     theta += 0.1;
 
     camera.position.x = radius * Math.sin( THREE.Math.degToRad( theta ) );
@@ -120,6 +72,5 @@ function render() {
 
     camera.updateMatrixWorld();
 
-    renderer.render( scene, camera );
-
+    effect.render( scene, camera );
 }
