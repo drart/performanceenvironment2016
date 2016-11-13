bb65983c9e16de06fb701f07cbbc986412135842
Adam Tindale <adam@adamtindale.com>
2016-11-13 09:39:03 -0500

    working out ableton push light controls

--- README.md
+++ README.md
@@ -5,8 +5,7 @@ TODO
 - MIDI (Push, LPD-8)
      - https://github.com/crosslandwa/push-wrapper
      - https://github.com/Ableton/push-interface (great docs)
-     - https://forum.ableton.com/viewtopic.php?f=55&t=193744&start=15 (writing to push screen)
-     - http://stackoverflow.com/questions/32277662/electron-sending-sysex-message-via-the-web-midi-api-crashes-renderer
+     - ~~https://forum.ableton.com/viewtopic.php?f=55&t=193744&start=15 (writing to push screen)~~
 - Sequences
 - shaderlib? https://threejs.org/examples/webgl_shaders_sky.html`
 
--- test/pushwrite/pushmiditest.js
+++ test/pushwrite/pushmiditest.js
@@ -4,6 +4,40 @@ var push = MidiStream('Ableton Push User Port')
 push.on('data', console.log);
 
 // push button light up
-push.write([144, 70, 100]);
+//push.write([144, 70, 127]);
+// left arrow light up
+//push.write([176, 85, 0]);
 // write to push screen!!!
-push.write([240,71,127,21,26,0,69,0,72,73,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,247]);
+//var x =[240,71,127,21,24,0,69,0,72,73,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,247]; 
+//push.write(x);
+
+// create a sysex message
+var stringtopushsysex = function(thestring, line){
+    line = (line === undefined) ? 24 : line + 24;
+    line = Math.min(line, 27);
+    line = Math.max(line, 24);
+
+    var sysexmessage = [240,71,127,21];
+    sysexmessage.push(line);
+    sysexmessage.push(0,69,0);
+    for(var i = 0; i < 68; i++){
+        if(i < mystring.length)
+            sysexmessage.push(mystring.charCodeAt(i));    
+        else
+            sysexmessage.push(32);
+    };
+    sysexmessage.push(247);
+
+    return sysexmessage;
+};
+
+var mystring = "f  adkjadflkjadflkja;lkjkljf a";
+var mymessage = stringtopushsysex(mystring, 1);
+push.write(mymessage);
+
+// light up beat repeat buttons
+for (var i = 36; i < 44; i++)
+    push.write([176, i, 90]);
+
+//console.log(typeof mymessage);
+//console.log(typeof x);
