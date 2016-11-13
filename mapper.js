3727fa3b8910da78cdfccc79312f04112e33b3db
Adam Tindale <adam@adamtindale.com>
2016-11-13 14:28:59 -0500

    push mappings worked out and started light interactions

--- mapper.js
+++ mapper.js
@@ -1,4 +1,3 @@
-
 var abletonpush = flock.midi.connection({
     // This should only be used if you know the port you want to use
     // ahead of time. Otherwise, the system.ports object should be bound to a UI
@@ -7,7 +6,6 @@ var abletonpush = flock.midi.connection({
     // Remember that the whole system is asynchronous, which means you have to wait
     // for the ready() event to get the current list of ports.
     openImmediately: true,
-    
     // This option is highly configurable. In the simplest case,
     // you can specify a "manufacturer" properity, a port "name" property (e.g. QUNEO), or both.
     // If you want more complex routings, you can specify "inputs" and "outputs" objects,
@@ -26,13 +24,17 @@ var abletonpush = flock.midi.connection({
         noteOn: function (msg) {
             abletonNoteOns(msg);
         },
-        
         noteOff: function () {
             synth.set("mod.mul.gate", 0);
         },
-        
         control: function (msg) {
             abletonCCs(msg);
+        },
+        pitchbend: function(msg) {
+            console.log(msg);
+        },
+        aftertouch: function(msg){
+            console.log(msg);
         }
     }
  
@@ -43,8 +45,8 @@ var abletonCCs = function(msg){
     // push knobs
     if (msg.number < 79 && msg.number > 70){
 	// glitchseq
-        glitches[msg.number - 71].prob = msg.value / 127;
-	 console.log(msg.value / 127);
+         glitches[msg.number - 71].prob = msg.value / 127;
+         console.log(msg.value / 127);
     }
     // play button
     if (msg.number === 85){
@@ -52,48 +54,40 @@ var abletonCCs = function(msg){
         buster.play();
         gibble.play();
         gobble.play();
-    }
 
-/*
-            var vibrato= msg.note / 32;
-            synth.set({
-                "mod.freq": vibrato
-            });
-*/
+        abletonpush.send([176, 85, 127]); // dim play light 
+    }
 };
 
 var abletonNoteOns = function(msg){
-    console.log(msg);
-    // push knob touches
-    if (msg.note < 12){
+    //knob touches
+    if (msg.note > 11){
+    
     }
-    // push buttons 
-    if(msg.note > 12){
-            var vel = msg.velocity/ 127;
-            synth.set({
-                "carrier.freq": flock.midiFreq(msg.note),
-                "mod.mul.gate": 1.0,
-                "mod.mul.sustain": vel
-            });
-            console.log(msg.note);
-            console.log(synth.get("carrier.freq"));
+    // grid buttons 
+    if(msg.note > 35 ){
+        var vel = msg.velocity/ 127;
+        synth.set({
+            "carrier.freq": flock.midiFreq(msg.note),
+            "mod.mul.gate": 1.0,
+            "mod.mul.sustain": vel
+        });
+        //console.log(msg.note);
+        //console.log(synth.get("carrier.freq"));
     }
 };
+
+// ---------------------------------
+// SETUP PUSH LIGHTS
+// ---------------------------------
+(function(){
+    setTimeout( function(){
+        abletonpush.send([176, 85, 1]); // dim play light 
+    }, 1000);
+})();
 
 /*
 // FUTURE? 
 // midi output? Activate button lights
 start by clearing them all
-var pushout = flock.midi.connection({
-    
-    openImmediately: true, 
-    ports: {
-        name: "Ableton Push User Output" 
-    },
-
-
-});
-
-pushout.send({})
-
 */
