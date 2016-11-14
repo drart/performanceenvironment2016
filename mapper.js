82b0cbfa2990e1d26d7300ebb7c72b1a9ef75f05
Adam Tindale <adam@adamtindale.com>
2016-11-13 20:44:10 -0500

    osc.js basics

--- mapper.js
+++ mapper.js
@@ -91,3 +91,26 @@ var abletonNoteOns = function(msg){
 // midi output? Activate button lights
 start by clearing them all
 */
+
+
+// ---------------------------------
+// SETUP OSC.JS
+// ---------------------------------
+var port = new osc.WebSocketPort({
+    url: "ws://localhost:8081"
+});
+
+port.on("message", function (oscMessage) {
+    //$("#message").text(JSON.stringify(oscMessage, undefined, 2));
+    console.log("message", oscMessage);
+});
+
+port.open();
+/*
+var sayHello = function () {
+    port.send({
+        address: "/hello",
+        args: ["world"]
+    });
+};
+*/
